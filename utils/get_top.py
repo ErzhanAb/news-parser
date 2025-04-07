dd5d5499f4f2ff6ed1f0ffbd43b9cd069cb63412
ErzhanAb <abdykadyrov.erzhan@gmail.com>
2025-04-07 11:28:23 +0600

    Update get_top.py

--- utils/get_top.py
+++ utils/get_top.py
@@ -8,7 +8,7 @@ def get_top(dataset: list, top: int) -> None:
     sorted_words = sorted(zip(unique_values, counts), key=lambda x: x[1], reverse=True)
     top_words = {
         'Value': [word[0] for word in sorted_words[:top]],
-        'Amount': [amount[1] for amount in sorted_words[:top]]
+        'Quantity': [quantity[1] for quantity in sorted_words[:top]]
     }
     df = pd.DataFrame(top_words)
     
